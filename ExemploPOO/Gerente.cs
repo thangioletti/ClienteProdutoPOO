dd2ad093b03dedde561b9fc08b151cb749c903b8
Tiago Henrique Angioletti <thangioletti@gmail.com>
2023-10-10 19:46:20 -0300

    Revert "Exemplo polimorfismo"

--- ExemploPOO/Gerente.cs
+++ ExemploPOO/Gerente.cs
@@ -10,7 +10,7 @@ namespace ExemploPOO
     {
         public override double CalcularSalarioLiquido()
         {
-            return this.Salario*0.7;
+            return this.Salario * 0.5;
         }
     }
 }
--- ExemploPOO/Program.cs
+++ ExemploPOO/Program.cs
@@ -5,28 +5,28 @@
         static void Main(string[] args)
         {
             //Login.Logar();
-            
             Funcionario funcionario = new Funcionario();
-            funcionario.Salario = 1000;
             funcionario.Nome = "Tiago";
+            funcionario.Salario = 1000;
 
             Estagiario estagiario = new Estagiario()
             {
+                Nome = "Joao",
                 Salario = 500,
-                Nome = "Joaozinho"
             };
 
             Gerente gerente = new Gerente()
             {
                 Nome = "Cleber",
-                Salario = 10000
+                Salario = 10000,
             };
 
-            Console.WriteLine($"O funcionario {funcionario.Nome} recebe liquido {funcionario.CalcularSalarioLiquido()}");
-            Console.WriteLine($"O estagiario {estagiario.Nome} recebe liquido {estagiario.CalcularSalarioLiquido()}");
-            Console.WriteLine($"O gerente {gerente.Nome} recebe liquido {gerente.CalcularSalarioLiquido()}");
+            //Console.WriteLine($"O Funcionario {funcionario.Nome} ganha liquido: R${funcionario.CalcularSalarioLiquido()}");
+            //Console.WriteLine($"O Estagiario {estagiario.Nome} ganha liquido: R${estagiario.CalcularSalarioLiquido()}");
+            //Console.WriteLine($"O Gerente {gerente.Nome} ganha liquido: R${gerente.CalcularSalarioLiquido()}");
 
-
+            Console.WriteLine($"2+2={Calculadora.Somar(2, 2)}");
+            Console.WriteLine($"2.5+2.5={Calculadora.Somar(2.5, 2.5)}");
         }
 
         public void Demonstrar(ICrud model)
